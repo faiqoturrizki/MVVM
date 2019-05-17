23f5cddf08479f14a8bf2e7751c6b2034f9a57b7
faiqoturrizki <rizkyfaix7@gmail.com>
2019-05-17 08:31:20 +0700

    Latihan MVVM2

--- app/src/main/java/com/example/mvvm/Model/Picture.java
+++ app/src/main/java/com/example/mvvm/Model/Picture.java
@@ -1,4 +1,11 @@
 package com.example.mvvm.Model;
 
-public class Picture {
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Picture implements Serializable {
+    @SerializedName("large") public String large;
+    @SerializedName("medium") public String medium;
+    @SerializedName("thumbnail") public String thumbnail;
 }
