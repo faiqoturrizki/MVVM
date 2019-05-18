f387ec1a61e9abe6a2fbee78c2336fb08b03ef34
faiqoturrizki <rizkyfaix7@gmail.com>
2019-05-18 08:48:12 +0700

    sabtu

---
+++ app/src/main/java/com/example/mvvm/Model/Data/PeopleViewModel.java
@@ -0,0 +1,4 @@
+package com.example.mvvm.Model.Data;
+
+public class PeopleViewModel {
+}
---
+++ app/src/main/java/com/example/mvvm/View/PeopleDetail.java
@@ -0,0 +1,15 @@
+package com.example.mvvm.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mvvm.R;
+
+public class PeopleDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people_detail);
+    }
+}
